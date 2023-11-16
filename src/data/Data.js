0905e6e639cbf176e66265bf7ea15dd47c72177b
Nitin Nitin <nitinww53@gmail.com>
2023-11-16 01:31:55 -0500

    Created Data file for the songs

---
+++ src/data/Data.js
@@ -0,0 +1,67 @@
+const songs = [
+  {
+    id: Math.random * Date.now(),
+    title: "11K",
+    artist: "Seedhe Maut",
+    image: "/assets/mp3Images/11k.jpg",
+    mp3: new Audio("/assets/mp3/11k.mp3"),
+  },
+  {
+    id: Math.random * Date.now(),
+    title: "Hasti Rahe Tu",
+    artist: "Paradox",
+    image: "/assets/mp3Images/hasti_rahe_tu.jpg",
+    mp3: new Audio("/assets/mp3/hasti_rahe_tu.mp3"),
+  },
+  {
+    id: Math.random * Date.now(),
+    title: "Hello",
+    artist: "Raftaar, Prabhdeep",
+    image: "/assets/mp3Images/hello.jpg",
+    mp3: new Audio("/assets/mp3/hello.mp3"),
+  },
+  {
+    id: Math.random * Date.now(),
+    title: "hola_amigo",
+    artist: "Kr$na, Seedhe Maut",
+    image: "/assets/mp3Images/hola_amigo.jpg",
+    mp3: new Audio("/assets/mp3/hola_amigo.mp3"),
+  },
+  {
+    id: Math.random * Date.now(),
+    title: "I Don't Miss That Life",
+    artist: "Seedhe Maut",
+    image: "/assets/mp3Images/i_dont_miss_that_life.jpg",
+    mp3: new Audio("/assets/mp3/i_dont_miss_that_life.mp3"),
+  },
+  {
+    id: Math.random * Date.now(),
+    title: "laado",
+    artist: "MC Square",
+    image: "/assets/mp3Images/laado.jpg",
+    mp3: new Audio("/assets/mp3/laado.mp3"),
+  },
+  {
+    id: Math.random * Date.now(),
+    title: "prarthana",
+    artist: "Kr$na",
+    image: "/assets/mp3Images/prarthana.jpg",
+    mp3: new Audio("/assets/mp3/prarthana.mp3"),
+  },
+  {
+    id: Math.random * Date.now(),
+    title: "thappad",
+    artist: "Prabhdeep",
+    image: "/assets/mp3Images/thappad.jpg",
+    mp3: new Audio("/assets/mp3/thappad.mp3"),
+  },
+  {
+    id: Math.random * Date.now(),
+    title: "Woh Raat",
+    artist: "Raftaar, Kr$na",
+    image: "/assets/mp3Images/woh_raat.jpg",
+    mp3: new Audio("/assets/mp3/woh_raat.mp3"),
+  },
+];
+
+export default songs;
