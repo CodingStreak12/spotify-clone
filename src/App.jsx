c66b463599573613419a796c35dce2b67a215c46
Nitin Nitin <nitinww53@gmail.com>
2023-11-16 01:21:50 -0500

    Deleted practice audio component and gitignored public folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Login from "./Components/Login/Login";
 import Signup from "./Components/Signup/Signup";
-import PracticeAudio from "./Components/practiceAudio/PracticeAudio";
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="signup" element={<Signup />} />
-        <Route path="practice" element={<PracticeAudio />} />
       </Routes>
     </BrowserRouter>
   );
